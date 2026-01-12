6221d942708f24ddd63b50682cad7bd03c05c861
penny <penny@hexmani.ac>
2026-01-12 11:15:40 -0500

    My awful non-flake and unstable package chud life

--- user.nix
+++ user.nix
@@ -59,6 +59,21 @@
   home-manager.useUserPackages = true;
   home-manager.users.penny =
     { pkgs, ... }:
+    let
+      pkgsUnstable = import <nixos-unstable> { };
+      oldZed = # fix later and use flakes or something
+        import
+          (pkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "64cd410b141f697c3db5ea25e110a9386fdc46f5";
+            sha256 = "sha256-8uhnBs+EqKW2raNZAV90yFXH9vcn81w2oC+Aso68Cuw=";
+          })
+          {
+            # fix later
+            inherit (pkgs) system;
+          };
+    in
     {
       nixpkgs.config = {
         allowUnfree = true;
@@ -66,49 +81,53 @@
       };
 
       # todo: not this shit
-      home.packages = with pkgs; [
-        bun
-        cabal-install
-        cargo
-        cider-2
-        crystal
-        discord
-        dmd
-        elixir
-        elixir-ls
-        erlang
-        erlang-language-platform
-        go
-        ghostty
-        gleam
-        ghc
-        htop
-        jetbrains-toolbox
-        jujutsu
-        kdePackages.kdeconnect-kde
-        kotlin
-        lmstudio
-        mpv
-        newsflash
-        nil
-        nixd
-        nodejs
-        opencode
-        openjdk
-        plex-desktop
-        plexamp
-        pnpm
-        ruby
-        rustc
-        signal-desktop
-        uv
-        vencord
-        vlang
-        vscodium
-        yarn
-        zed-editor
-        zig
-      ];
+      home.packages =
+        with pkgs;
+        [
+          bun
+          cabal-install
+          cargo
+          cider-2
+          crystal
+          discord
+          dmd
+          elixir
+          elixir-ls
+          erlang
+          erlang-language-platform
+          go
+          ghostty
+          ghc
+          htop
+          jetbrains-toolbox
+          jujutsu
+          kdePackages.kdeconnect-kde
+          kotlin
+          lmstudio
+          mpv
+          newsflash
+          nil
+          nixd
+          nodejs
+          opencode
+          openjdk
+          plex-desktop
+          plexamp
+          pnpm
+          ruby
+          rustc
+          signal-desktop
+          uv
+          vencord
+          vlang
+          vscodium
+          yarn
+          oldZed.zed-editor
+          zig
+        ]
+        ++ (with pkgsUnstable; [
+          gleam
+        ]);
 
       home.stateVersion = "25.11";
     };
